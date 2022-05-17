7da348c69c80d3f15ecaa2493aa91b888810cb60
Talha Riaz <publishappstore74@gmail.com>
2022-05-17 23:40:04 +0500

    initial commit

---
+++ app/src/main/java/com/tfinity/basicapp_a020/MainActivity.java
@@ -0,0 +1,22 @@
+package com.tfinity.basicapp_a020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button b1,b2,b3,b4.b5.b6;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
