7314304d51265c43d820f3f0707367aa6bb82622
Talha Riaz <publishappstore74@gmail.com>
2022-05-18 00:33:50 +0500

    image load in second page

---
+++ app/src/main/java/com/tfinity/basicapp_a020/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.tfinity.basicapp_a020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
